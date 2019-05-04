0455e33bda7c6f317c83005e42e44b664d5ee70f
vamsiKrishna <vamsi@vamsiKrishnas-MacBook-Pro.local>
2019-05-04 18:43:35 +0530

    Landing Page Responsive , yet to add bottom btn action

--- fe/sarc/src/views/Home.vue
+++ fe/sarc/src/views/Home.vue
@@ -4,24 +4,23 @@
       <img class="clipSvg" src="../assets/a.png" style="" alt="">
     </div>
     <div class="headerImg">
-      <img src="../assets/bits-logo.png" style="width: 200px; height: auto;position: absolute;top: 30px;left: 30px;" alt="">
+      <img class="logo" src="../assets/logo.png" style=";position: absolute;top: 30px;left: 30px;" alt="">
     </div>
     <div class="mainCont"> 
-      <p style="font-size: 50px;font-weight: 600;font-family: 'Product Sans'; padding-top: 0px;padding-bottom: 0px;margin: 0px;color: #32217C;" >Website</p> <br>
-      <p style="font-size: 80px;font-weight: 900;line-height: 40px;font-family: 'Product Sans';padding-top: 0px;padding-bottom: 0px;margin: 0px;">Comming</p> <br>
-      <p style="font-size: 80px;font-weight: 900;font-family: 'Product Sans';padding-top: 0px;padding-bottom: 0px;margin: 0px;margin-top: -20px;">Soon</p> <br>
-      <p style="width: 30vw;height: auto;font-weight: 600;">Place holder text intro about the site Place holder text intro about the site Place holder text intro about the site Place holder text intro about the site Place holder text intro about the site Place holder text intro about the site Place holder text intro about the site Place holder text intro about the site Place holder text intro about.</p>
+      <p class="website" style="font-weight: 600;font-family: 'Product Sans'; padding-top: 0px;padding-bottom: 0px;margin: 0px;color: #32217C;" >Website</p> <br>
+      <p class="comming" style="font-weight: 900;font-family: 'Product Sans';padding-top: 0px;padding-bottom: 0px;margin: 0px;">Comming</p> <br>
+      <p class="soon" style="font-weight: 900;font-family: 'Product Sans';padding-top: 0px;padding-bottom: 0px;margin: 0px;">Soon</p> <br>
+      <p class="entryText" style="height: auto;font-weight: 600;">Place holder text intro about the site Place holder text intro about the site Place holder text intro about the site Place holder text intro about the site Place holder text intro about the site Place holder text intro about the site Place holder text intro about the site Place holder text intro about the site Place holder text intro about.</p>
     </div>
     <div class="footerLeft">
-      <svg xmlns="http://www.w3.org/2000/svg" width="800" height="200" viewBox="0 0 800 200">
-        <path id="Path_4" data-name="Path 4" d="M799.8,1124.587c2.1-.191-12.655-49.282-25.9-76.839s-33.776-50.518-56.981-61.437-32.14-16.405-64.993-20.584-42.3-.765-56.24,0c-32.737,1.8-67.894,13.118-88.343,20.584-28.882,10.545-61.372,22.211-124.527,56.987s-125.123,30.333-125.123,30.333-55.759-3-126.981-87.32S0,928.554,0,928.554v196.033Z" transform="translate(0 -900)" fill="#32217C" stroke="none" stroke-width="1"/>
-      </svg>
+      <img src="../assets/bottom.png" style="width: 50vw; height: auto;position: absolute; left: 0px; bottom: 0px;" alt="">
     </div>
     <div class="footerRight">
-      <img src="../assets/facebook.svg" style="width: 25px; height: 25px;margin-left:50px;cursor: pointer" alt="">
-      <img src="../assets/instagram.svg" style="width: 25px; height: 25px;margin-left:50px;cursor: pointer" alt="">
-      <img src="../assets/twitter.svg" style="width: 25px; height: 25px;margin-left:50px;cursor: pointer" alt="">
-      <img src="../assets/whatsapp.svg" style="width: 28px; height: 28px;margin-left:50px;transform: translateY(-1.5px);cursor: pointer" alt="">
+      <img class="icon" src="../assets/facebook.svg" style="" alt="">
+      <img class="icon" src="../assets/instagram.svg" style="" alt="">
+      <img class="icon" src="../assets/twitter.svg" style="" alt="">
+      <img class="icon" src="../assets/whatsapp.svg" style="" alt="">
+      <img class="hiddenicon" src="../assets/menu.svg" style="" alt="">
     </div>
   </div>
 </template>
@@ -30,6 +29,11 @@
 .home {
   overflow: hidden;
 }
+.logo {
+  width: auto; 
+  height: 70px;
+}
+
 .clipSvg {
   position: absolute;
   width: 50vw;
@@ -55,15 +59,152 @@
   text-align: left;
   padding-left: 50px;
 }
+.entryText {
+  width: 40vw;
+}
+.icon {
+  width: 25px; 
+  height: 25px;
+  margin-left:50px;
+  cursor: pointer
+}
+.hiddenicon{
+  width: 22px; 
+  height: 22px;
+  margin-left:50px;
+  cursor: pointer;
+  display: none;
+}
+.website {
+  -webkit-animation: fadeAnimation 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+	        animation: fadeAnimation 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+  animation-delay: 1s;
+  font-size: 50px;
+}
+.comming {
+    -webkit-animation: fadeAnimation 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+	        animation: fadeAnimation 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+  font-size: 80px;
+  line-height: 40px;
+}
+.soon{
+    -webkit-animation: fadeAnimation 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+	        animation: fadeAnimation 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+    animation-delay: 0s;
+  font-size: 80px;
+  margin-top: -20px;
+}
+.entryText {
+    -webkit-animation: fadeAnimation 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+	        animation: fadeAnimation 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+    animation-delay: 1.5s;
+}
 .footerRight {
   position: absolute;
-  top: calc(100vh - 70px);
-  right: 40px;
+  top: calc(100vh - 50px);
+  right: 60px;
   width: auto;
   height: 200px;
   display: flex;
   flex-direction: row;
+  -webkit-animation: fadeAnimation 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+      animation: fadeAnimation 1.2s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
+  animation-delay: 1.5s;
 }
+
+/* Animation */
+
+@-webkit-keyframes fadeAnimation {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes fadeAnimation {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+/*  media queries  */
+
+@media only screen and (max-width: 968px) {
+  .entryText {
+    width: 55vw;
+  }
+  .icon{
+    width: 22px;
+    height: 22px;
+  }
+}
+
+@media only screen and (max-width: 820px) {
+  .icon{
+    display: none;
+  }
+  .hiddenicon{
+    display: block;
+  }
+}
+
+@media only screen and (max-width: 685px) {
+  /* remove bottom img display */
+  .footerLeft{
+    display: none;
+  }
+  .website{
+    font-size: 35px;
+  }
+  .comming{
+    font-size: 50px;
+    line-height: 30px;
+  }
+  .soon{
+    font-size: 50px;
+  }
+  /* reduce logo size */
+  .logo{
+    height: 50px;
+  }
+  .clipSvg{
+    width: 65vw;
+  }
+}
+
+@media only screen and (max-width: 568px) {
+  /* remove side image display */
+  .clipSvg{
+    display: none;
+  }
+  .website{
+    font-size: 25px;
+  }
+  .comming{
+    font-size: 40px;
+    line-height: 15px;
+  }
+  .soon{
+    font-size: 40px;
+    line-height: 15px;
+  }
+  /* reduce logo size */
+  .logo{
+    height: 40px;
+  }.entryText{
+    width: 80vw;
+  }
+  .hiddenicon{
+    width: 30px;
+    height: 30px;
+    margin-right: -30px;
+  }
+}
+
 </style>
 
 <script>
